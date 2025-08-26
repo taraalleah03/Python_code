34010b3cd0e7442845653d12418beadc7cbb9594
tarahere <mtaraalleah@gmail.com>
2025-08-26 09:24:48 +0300

    if else practice

---
+++ coffeeshopex.py
@@ -0,0 +1,9 @@
+money = float(input("How much money you got?: "))
+
+if money >= 5:
+    print("You have enough money to buy a coffee!")
+else:
+    print("You don't have enough money to buy a coffee :(")
+
+print("Have a nice day!")
+
