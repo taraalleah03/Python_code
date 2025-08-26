ff9f25c31b926fe706df69213254fabc73e8b705
tarahere <mtaraalleah@gmail.com>
2025-08-26 09:41:41 +0300

    26.08.2025 class examples

---
+++ boolean and operators ex.py
@@ -0,0 +1,11 @@
+a = True
+b = False
+
+if a and b == True:
+    print("a and b are true")
+
+if a or b == True:
+    print("a or b are true")
+
+if a == True or b == False:
+    print("a is not", not a, "\nb is not", not b )
---
+++ catpractice.py
@@ -0,0 +1,8 @@
+cat = input("What is your cat's name? ")
+dog= input("What is your dog's name? ")
+
+if cat==dog:
+    print("Your cat and god have the same name!")
+else :
+    print("Your dog's name is", dog)
+    print("and your cat's name is", cat)
