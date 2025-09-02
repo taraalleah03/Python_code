b96f3653cbce2545a1f73f0c95b3b41d175121d6
tarahere <mtaraalleah@gmail.com>
2025-09-03 00:08:15 +0300

    module 4 excercise 5

--- Excercises4-while loops/username-and-password.py
+++ Excercises4-while loops/username-and-password.py
@@ -4,11 +4,24 @@ password = input("Create a password: ")
 loginuser = input("Enter your username: ")
 loginpassword = input("Enter your password: ")
 
+tries = 1
+
 while loginuser != user or loginpassword != password:
 
-    print("Wrong username or password, try again")
-    loginuser = input("Enter your username: ")
-    loginpassword = input("Enter your password: ")
+    while tries < 5:
+        print("Wrong username or password, try again")
+
+        loginuser = input("Enter your username: ")
+        loginpassword = input("Enter your password: ")
+
+        tries += 1
+
+        if tries == 5:
+            print("Access Denied")
+            break
+
+        elif loginuser == user and loginpassword == password:
+            print("Welcome")
+            break
 
 
-print("Welcome ")
