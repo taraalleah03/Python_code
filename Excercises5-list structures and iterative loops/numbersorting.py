ab087ce8a165c3701bee98f18abed2f2b95c10db
tarahere <mtaraalleah@gmail.com>
2025-09-04 14:12:51 +0300

    module 5 excercise 2 second attempt

--- Excercises5-list structures and iterative loops/numbersorting.py
+++ Excercises5-list structures and iterative loops/numbersorting.py
@@ -15,14 +15,6 @@ numlist.remove(numlist[0]) #this is to remove the blank space that ends the whil
 intlist = list(map(int, numlist))
 
 dnumlist = sorted(numlist, reverse= True)
+print(dnumlist)
 
-l = len(dnumlist) #this is to check how many elements are in the list using the built in len() function
-i = 0
-
-if l > 5: #it makes more sense to me to use while in this situation
-    while i < 5 :
-        print(dnumlist[i])
-else:
-    print(dnumlist)
-
-#I tested this out with multiple numbers with carrying digits ,and it doesn't seem to work that well, it works best with 1 digit numbers, is it cuz they're string?
+#I tested this out with multiple numbers with varying digits ,and it doesn't seem to work that well, it works best with 1 digit numbers, is it cuz they're string?
