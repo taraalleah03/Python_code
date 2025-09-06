ae075dd4f537c449fd652c77d34045e8e9516129
tarahere <mtaraalleah@gmail.com>
2025-09-06 21:57:15 +0300

    trial

--- Excercises5-list structures and iterative loops/primenumchecker.py
+++ Excercises5-list structures and iterative loops/primenumchecker.py
@@ -1,7 +1,9 @@
 num = int(input("Enter a number: "))
 
-for i in range(2, num+1):
-    if i % num == 0:
-        print(num,"is a prime number")
-    elif num > 1:
-        print(num,"is not a prime number")
+for i in range(2,num+1):
+
+    if num%1 == 0:
+        print(num, "is not a prime number")
+        break
+    else:
+        print(num, "is a prime number")
