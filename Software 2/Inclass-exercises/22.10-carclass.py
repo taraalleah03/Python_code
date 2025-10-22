83d698aaa2b402a7e5f1633794bc30f618e3a454
tarahere <mtaraalleah@gmail.com>
2025-10-22 14:31:56 +0300

    mileage

---
+++ Software 2/Inclass-exercises/22.10-carclass.py
@@ -0,0 +1,18 @@
+class Car:
+
+    def __init__(self,brand,color,mileage):
+        self.brand = brand
+        self.color = color
+        self.mileage = mileage
+
+    def drive(self,distance):
+        self.mileage = self.mileage + distance
+        print(f"The {self.brand} that is {self.color} has drove a total of {self.mileage} miles.")
+        return
+
+car1 = Car('Toyota','red',0)
+car2 = Car('Audi','blue',0)
+
+car1.drive(10)
+car2.drive(20)
+car1.drive(10)
