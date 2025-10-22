cbb00a9566421c8b9cb851d3b5e6302c42b81a74
tarahere <mtaraalleah@gmail.com>
2025-10-22 14:47:26 +0300

    color

--- Software 2/Inclass-exercises/22.10-carclass.py
+++ Software 2/Inclass-exercises/22.10-carclass.py
@@ -10,9 +10,17 @@ class Car:
         print(f"The {self.brand} that is {self.color} has drove a total of {self.mileage} miles.")
         return
 
+    def repaint(self,color):
+        print(f"The {self.brand} that was {self.color} is now {color}")
+        self.color = color
+
 car1 = Car('Toyota','red',0)
 car2 = Car('Audi','blue',0)
 
 car1.drive(10)
 car2.drive(20)
 car1.drive(10)
+car2.repaint('yellow')
+car2.repaint('pink')
+
+
