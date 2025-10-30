e709e733aa019675116010898e4279302535c619
tarahere <mtaraalleah@gmail.com>
2025-10-31 01:21:08 +0200

    exercise

---
+++ Software 2/Exercises10_association/10.1-3_elevatorclass-buildingclass.py
@@ -0,0 +1,11 @@
+class Elevator:
+    def __init__(self, bottom, top):
+        self.bottom = bottom
+        self.top = top
+
+    def go_to_floor(self,elevator):
+
+    def floor_up(self,elevator):
+
+    def floor_down(self,elevator):
+
