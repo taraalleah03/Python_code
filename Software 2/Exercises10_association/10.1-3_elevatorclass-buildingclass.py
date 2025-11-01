68379108dce80f979f4516deab05238be7857f64
tarahere <mtaraalleah@gmail.com>
2025-11-01 17:38:38 +0200

    exercise

--- Software 2/Exercises10_association/10.1-3_elevatorclass-buildingclass.py
+++ Software 2/Exercises10_association/10.1-3_elevatorclass-buildingclass.py
@@ -2,10 +2,19 @@ class Elevator:
     def __init__(self, bottom, top):
         self.bottom = bottom
         self.top = top
+        self.elevator = bottom #this is where the elevator is currently
 
-    def go_to_floor(self,elevator):
+    def floor_up(self):
+        self.top = elevator + 1
 
-    def floor_up(self,elevator):
+    def floor_down(self):
+        self.bottom = elevator - 1
 
-    def floor_down(self,elevator):
+    def go_to_floor(self, elevator):
+        if elevator < self.top:
+            self.floor_up(elevator)
+        elif elevator > self.bottom:
+            self.floor_down(elevator)
+        else:
+            print ("You have made it to your destination!")
 
