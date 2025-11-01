1bf06a8ad08a6a9dbc4e87b38f928ecb13a81eb7
tarahere <mtaraalleah@gmail.com>
2025-11-01 18:15:54 +0200

    exercise

--- Software 2/Exercises10_association/10.1-3_elevatorclass-buildingclass.py
+++ Software 2/Exercises10_association/10.1-3_elevatorclass-buildingclass.py
@@ -7,10 +7,12 @@ class Elevator:
     def floor_up(self):
         if self.elevator < self.top:
             self.elevator += 1
+            print(f"The elevator has moved one floor up. Now on floor {self.elevator}")
 
     def floor_down(self):
         if self.bottom < self.elevator:
             self.elevator -= 1
+            print(f"The elevator has moved one floor down. Now on floor {self.elevator}")
 
     def go_to_floor(self, floor_togo):
         while self.elevator < floor_togo:
@@ -20,13 +22,24 @@ class Elevator:
         print ("You have made it to your destination!")
 
 class Building:
-    def __init__(self, bottom, top, num_floors):
-        self.floors = []
 
-        for i in range(num_floors):
-            new_floor = Elevator(bottom, top)
-            self.floors.append(new_floor)
+    def __init__(self, bottom, top, num_elevators):
+        self.elevators = []
+        for i in range(num_elevators):
+            new_elevator = Elevator(bottom, top)
+            self.elevators.append(new_elevator)
 
+    def run_elevator(self, num, destination):
+        self.elevators[num].go_to_floor(destination)
 
+    def fire_alarm(self):
+        print("Fire alarm warning! All elevators must move to the bottom floor!")
+        for floor in self.elevators:
+            floor.go_to_floor(floor.bottom)
 
+building = Building(0, 10, 3)
+building.run_elevator(1, 5)
+#building.run_elevator(10,5)
+building.run_elevator(2,7)
+building.fire_alarm()
 
