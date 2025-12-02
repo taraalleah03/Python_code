3a7b930a190e76e359fcedb272f5e9c97e3d372b
tarahere <mtaraalleah@gmail.com>
2025-12-02 14:31:20 +0200

    exercise

--- Software 2/Exercises13_setting-up-backendservice-with-interface/13.2_ICAO.py
+++ Software 2/Exercises13_setting-up-backendservice-with-interface/13.2_ICAO.py
@@ -11,9 +11,26 @@ connection = mariadb.connect(
     database='flight_game',
     autocommit=True)
 
-@app.route('/airport/<ICAO>')
-def airport(ICAO):
+@app.route('/airport/<ident>', strict_slashes=False)
+def airport(ident):
     cursor = connection.cursor()
+    cursor.execute("SELECT name, municipality FROM airport WHERE ident = ?", (ident,))
+    result = cursor.fetchone()
+    cursor.close()
 
-    sql = "SELECT name, municipality FROM airports WHERE ident=?"
+    if result:
+        name, municipality = result
+        response = {
+            'Ident': ident, #There doesn't seem to be an ICAO in our existing DB
+            'Name': name,
+            'Location': municipality,
+        }
+    else:
+        response = {
+            'Error': 'Invalid identifier',
+        }
 
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
