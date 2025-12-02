b79c380834b2b34c063e5fffbb49da1a9111f890
tarahere <mtaraalleah@gmail.com>
2025-12-02 12:53:24 +0200

    exercise

--- Software 2/Exercises13_setting-up-backendservice-with-interface/13.1_Flask.py
+++ Software 2/Exercises13_setting-up-backendservice-with-interface/13.1_Flask.py
@@ -1,32 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, json, jsonify
 
 app = Flask(__name__)
-@app.route('/prime_number')
+@app.route('/prime_number/<int:num>')
 
-def check_prime():
-    args = request.args
-    num = int(args.get["num"])
+def check_prime(num):
     answer = True
 
     if num == 0 or num == 1:
-        print(num, "is not a prime number")
+        answer = False
 
-    elif num > 1:
+    else:
 
         for i in range(2, num):
             if (num % i) == 0:
-                answer = True
+                answer = False
                 break
-
-        else:
-            answer = True
-
-    else:
-        answer = False
 
     response = {"Number": num,
                 "isPrime": answer
                 }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
